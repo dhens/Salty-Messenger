0cd41727d6faa0bced4e6d7b2e26f05b938eb585
dhens <dawsonhensel@gmail.com>
2020-04-05 18:26:27 -0700

    removed routes and code for legacy project

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -8,77 +8,22 @@ var $noteList = $(".list-container .list-group");
 var activeNote = {};
 
 // A function for getting all notes from the db
-var getNotes = function() {
+var getMessages = function() {
   return $.ajax({
-    url: "/api/notes",
+    url: "/api/msgs",
     method: "GET"
   });
 };
 
 // A function for saving a note to the db
-var saveNote = function(note) {
+var sendMessage = function(msg) {
   return $.ajax({
-    url: "/api/notes",
-    data: note,
+    url: "/api/msg",
+    data: msg,
     method: "POST"
   });
 };
 
-// A function for deleting a note from the db
-var deleteNote = function(id) {
-  return $.ajax({
-    url: "api/notes/" + id,
-    method: "DELETE"
-  });
-};
-
-// If there is an activeNote, display it, otherwise render empty inputs
-var renderActiveNote = function() {
-  $saveNoteBtn.hide();
-
-  if (activeNote.id) {
-    $noteTitle.attr("readonly", true);
-    $noteText.attr("readonly", true);
-    $noteTitle.val(activeNote.title);
-    $noteText.val(activeNote.text);
-  } else {
-    $noteTitle.attr("readonly", false);
-    $noteText.attr("readonly", false);
-    $noteTitle.val("");
-    $noteText.val("");
-  }
-};
-
-// Get the note data from the inputs, save it to the db and update the view
-var handleNoteSave = function() {
-  var newNote = {
-    title: $noteTitle.val(),
-    text: $noteText.val()
-  };
-
-  saveNote(newNote).then(function(data) {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
-};
-
-// Delete the clicked note
-var handleNoteDelete = function(event) {
-  // prevents the click listener for the list from being called when the button inside of it is clicked
-  event.stopPropagation();
-
-  var note = $(this)
-    .parent(".list-group-item")
-    .data();
-
-  if (activeNote.id === note.id) {
-    activeNote = {};
-  }
-
-  deleteNote(note.id).then(function() {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
 };
 
 // Sets the activeNote and displays it
--- server.js
+++ server.js
@@ -17,51 +17,7 @@ app.get('/room', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/room.html"));
 })
 
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'));
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = uniqid();
-    const newNote = req.body;
-    
-    fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
-        // Error is thrown if you delete all notes then try to set savedNotes equal to db.json data 
-        // This handles an empty db.json file
-        let savedNotes;
-        try {
-            savedNotes = JSON.parse(data);
-        } catch {
-            savedNotes = [];
-        }
-        savedNotes.push(newNote);
-        writeDatabase(savedNotes);
-        console.log(`Added note with id ${req.body.id}`);
-        res.send(`Added note with id ${req.body.id}`);
-    });
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
-        const savedNotes = JSON.parse(data);
-
-        // Create new db without the note with the ID we specify
-        const newNotes = savedNotes.filter(item => {
-            return item.id !== req.params.id;
-        });
-        writeDatabase(newNotes);
-        console.log(`${req.params.id} deleted`)
-        res.send(`Note ${req.params.id} deleted`);
-    });
-
-});
-
-function writeDatabase(data) {
-    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(data), err => {
-        if (err) throw err;
-    });
-}
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    console.log(`🧂 Server started on port ${PORT}`);
 });
