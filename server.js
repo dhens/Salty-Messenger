2580b01960f65491817bfff1f88545d598ba89b6
Dawson Hensel <dawsonhensel@gmail.com>
2020-05-29 09:52:29 -0700

    made imports in serverjs more readable and turned off x-powered-by header

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const app = require('express')();
 const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
-
+app.disable('x-powered-by'); // Prevents server from identifying itself for potential attackers
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
